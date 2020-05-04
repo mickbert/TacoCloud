8161cb78e3409268664c9c5bdf2fd239bc4b2e60
Mick <MickBert@posteo.net>
2020-05-04 18:30:02 +0200

    Aggiungi il repository a OrderController

--- src/main/java/com/mibe/tacocloud/web/OrderController.java
+++ src/main/java/com/mibe/tacocloud/web/OrderController.java
@@ -1,6 +1,7 @@
 package com.mibe.tacocloud.web;
 
 import com.mibe.tacocloud.Order;
+import com.mibe.tacocloud.data.OrderRepository;
 
 import javax.validation.Valid;
 
@@ -10,10 +11,19 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 @Controller
 @RequestMapping("/orders")
+@SessionAttributes("order")
 public class OrderController {
+    OrderRepository orderRepo;
+
+    public OrderController(OrderRepository anOrderRepo) {
+        orderRepo=anOrderRepo;
+    }
+    
     @GetMapping("/current")
     public String orderForm(Model aModel) {
         aModel.addAttribute("order",new Order());
@@ -21,11 +31,15 @@ public class OrderController {
     }
         
     @PostMapping
-    public String processOrder(@Valid Order aOrder, Errors aResult) {
+    public String processOrder(@Valid Order aOrder, Errors aResult,
+                               SessionStatus aSessionStatus) {
         if(aResult.hasErrors()) {
             System.err.printf("OrderController:26 %s%n",aResult);
             return "orderForm";
         }
+        orderRepo.save(aOrder);
+        aSessionStatus.setComplete();
+        
         System.err.printf("process order form: %s%n",aOrder);
         return "redirect:/";
     }
